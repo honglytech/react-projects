93c46ee2d46b155387e8b8d6cd0899ae7b3be779
Hong Ly <lyhong.rupp@gmail.com>
2023-03-01 12:30:49 +1100

    to-do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import TodoList from "./TodoList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React Projects</p>
-        <a
-          className="App-link"
-          href="https://honglytech.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          About Me
-        </a>
-      </header>
+    <div>
+      <TodoList />
     </div>
   );
-}
+};
 
 export default App;
